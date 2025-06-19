cd49b70f5a7868d08a343ffd16a1731f3eb84275
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-06-19 21:55:27 +0200

    Attempt to identify macOS GHA issue - take 2

--- R/utils,cluster.R
+++ R/utils,cluster.R
@@ -210,7 +210,7 @@ session_info <- function(pkgs = getOption2("parallelly.makeNodePSOCK.sessionInfo
 
     ## Capabilities, etc
     capabilities = capabilities(),
-#    l10n = l10n_info(),
+    l10n = l10n_info(),
 
     ## Build environment
 #    extSoftVersion = extSoftVersion(),
