6cf4ec50f9cd95ad768cdb509eb12abc05264f4b
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-12-10 14:39:43 -0800

    HELP: Expand help(freePort) + add examples

--- R/ports.R
+++ R/ports.R
@@ -4,7 +4,10 @@
 #' Zero or more TCP ports in \[0, 65535\] to scan.
 #' If `"random"`, then a random set of ports is considered.
 #' If `"auto"`, then the port given by environment variable
-#' \env{R_PARALLEL_PORT} is used, which may also specify `random`.
+#' \env{R_PARALLEL_PORT} is used, which may also specify
+#' `"random"`. If \env{R_PARALLEL_PORT} is not set, the default
+#' is `"random"`. The `"auto"` alternative is used for full
+#' backward compatibility with the **parallelly** package.
 #'
 #' @param default (integer) `NA_integer_` or a port to returned if
 #' an available port could not be found.
@@ -14,9 +17,44 @@
 #' @param randomize (logical) If TRUE, `ports` is randomly shuffled
 #' before searched.  This shuffle does _not_ forward the RNG seed.
 #'
+#' @section Validation of TCP port returned:
+#' This function attempts to validate that the TCP port returned is
+#' available and can be be opened. Thus, it is likely that this port
+#' is available momentarily after `freePort()` returns. However, due
+#' to race conditions, there is always a risk that the port is taken
+#' by another process on the same system after `freePort() returns
+#' but before, say, `makeClusterPSOCK()`, opens it.
+#'
+#' @section Controlling the random set of TCP ports:
+#' For backward compatibility with the **parallel** package, the default
+#' set of of ports `freePort("random")`, or `freePort("auto")` with
+#' environment variable `R_PARALLEL_PORT=random` samples from is
+#' `11000:11999`.
+#' The set can be overriden by setting environment variable
+#' `R_PARALLELLY_RANDOM_PORTS`,  e.g.
+#' `R_PARALLELLY_RANDOM_PORTS="10001:10010,11000:54000"`.
+#'
 #' @return
-#' Returns an integer representing the first port among `ports` that
-#' can be opened.  If none can be opened, then `default` is returned.
+#' Returns an integer representing the first port among `ports` _that
+#' can be opened_. If none can be opened, then `default` is returned.
+#' 
+#' @examples
+#' # Get a random, free TCP port in 1024:65535
+#' port <- freePort()
+#' message("A free TCP port: ", port)
+#'
+#' # Get a random, free TCP port in 11000:11999, which is what
+#' # parallelly::makeClusterPSOCK() and parallel::makePSOCKcluster()
+#' # default to (but the latter does not make sure it is available)
+#' port <- freePort("random")
+#' message("A free TCP port: ", port)
+#'
+#' # Customize the range of ports to sample from to 30000:50000
+#' oenv <- Sys.getenv("R_PARALLELLY_RANDOM_PORTS")
+#' Sys.setenv(R_PARALLELLY_RANDOM_PORTS = "30000:50000")
+#' port <- freePort("random")
+#' Sys.setenv(R_PARALLELLY_RANDOM_PORTS = oenv)
+#' message("A free TCP port: ", port)
 #'
 #' @export
 freePort <- function(ports = 1024:65535, default = "random", randomize = TRUE) {
